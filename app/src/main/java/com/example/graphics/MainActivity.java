4ef3066f80fe4782f79e29df26f1cc4afeff7c66
Annelís <juanysacalxot376@gmail.com>
2022-03-01 10:35:45 -0600

    Modificacion para la entrada de texto en activity_main

--- app/src/main/java/com/example/graphics/MainActivity.java
+++ app/src/main/java/com/example/graphics/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.graphics;
 
+import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //metodo para que haga algo cuando le de click
+
+    public void onClick(View view){
+
     }
 }
